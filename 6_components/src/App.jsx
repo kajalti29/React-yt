f781947b3b5ccb6449d5a0617e7d50c2c2ba0de8
kajalti@29 <kajaltisirame@gmail.com>
2025-11-10 06:50:25 +0530

    name commit

--- 6_components/src/App.jsx
+++ 6_components/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const App = () => {
+  
+  const user = 'kajalti'
+  const age = 89
+
   return (
-    <div className='card'>
+    <div class='card'>
       <h1>Saarthak sharma</h1>
       <p>lorem ipsum dolor sit amet consectur adipicing elit. Deserun, vitae!</p>
+
+      <h2>Hello, Guys I am {user}</h2>
+      <h3>and my age {age} year old</h3>
     </div>
   )
 }
