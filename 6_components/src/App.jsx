5ef92ab84066029c013e119e22541025b5a0bf2c
kajalti@29 <kajaltisirame@gmail.com>
2025-11-10 06:21:44 +0530

    name commit

---
+++ 6_components/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class='card'>
+      <h1>Saarthak sharma</h1>
+      <p>lorem ipsum dolor sit amet consectur adipicing elit. Deserun, vitae!</p>
+    </div>
+  )
+}
+
+export default App
