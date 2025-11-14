788585493d69c934d3be6d0882e0c8d359258b39
kajalti@29 <kajaltisirame@gmail.com>
2025-11-14 22:55:08 +0530

    card commit

--- React/Practice/Folder/01_firstcode/src/App.jsx
+++ React/Practice/Folder/01_firstcode/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./App.css";   // CSS ko import karna mat bhoolna
 
 function App() {
   return (
-    <div className="insta-post">
-    <div>
-      
+    <div className="card">
+      <img src="/picture.jpg" alt="image" />
+      <h1>Kajalti Sirame</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+</p>
+<button>View profile</button>
     </div>
-      
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
