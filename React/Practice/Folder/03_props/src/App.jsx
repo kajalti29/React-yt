4a0e556f093d911cf617815316da3052175d100b
kajalti@29 <kajaltisirame@gmail.com>
2025-11-16 11:54:37 +0530

    code commit

---
+++ React/Practice/Folder/03_props/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='profile'>
+    <img src="public\images.jpg" alt="Profile" />
+    <a href='#'></a>
+    </div>
+  )
+}
+
+export default App
