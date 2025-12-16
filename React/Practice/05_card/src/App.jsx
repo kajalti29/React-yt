380353a8c86c9da9b46699fdd7d887d7836a7889
kajalti@29 <kajaltisirame@gmail.com>
2025-12-16 09:09:18 +0530

    card commit

---
+++ React/Practice/05_card/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>  
+      <div className="card">
+        <img src="src\assets\WhatsApp Image 2025-12-14 at 3.09.52 PM.jpeg" alt="profile pic" /> 
+        <h1>Kajalti Sirame</h1> 
+        <p>Lorem ipsum dolor sit amet consectur adipisicing elit.</p>
+        <button>View profile</button>
+      </div>
+    </div> 
+  )
+}
+
+export default App
