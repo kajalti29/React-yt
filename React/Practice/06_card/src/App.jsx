8e1305196a916fed67ad4980554dfccfa5f4a6f5
kajalti@29 <kajaltisirame@gmail.com>
2025-12-25 16:36:47 +0530

    radhika commit

---
+++ React/Practice/06_card/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <img src="src\assets\Images.jpg" alt="profile pic" /> 
+        <h1>Radhika</h1> 
+        <p>Lorem ipsum dolor sit amet consectur adipisicing elit.</p>
+        <button>View profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
