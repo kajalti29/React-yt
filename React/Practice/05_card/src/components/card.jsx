179b25c894750bf808a345a019d9e80dd0b8c18e
kajalti@29 <kajaltisirame@gmail.com>
2025-12-25 13:27:14 +0530

    card commit

--- React/Practice/05_card/src/components/card.jsx
+++ React/Practice/05_card/src/components/card.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import Card from './components/card.jsx'  
 
-const App = () => {
+const card = () => {
   return (
-    <div className='parent'>  
-      {/* <div className="card">
+    <div>
+      <div className="card">
         <img src="src\assets\Kajalti (2).png" alt="profile pic" /> 
         <h1>Kajalti Sirame</h1> 
         <p>Lorem ipsum dolor sit amet consectur adipisicing elit.</p>
         <button>View profile</button>
-      </div> */}
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div> 
+      </div>
+    </div>
   )
 }
-
-export default App
+  
+export default card
