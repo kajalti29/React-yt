ed22294072129de576f4bdfb4ed131d940f56141
kajalti@29 <kajaltisirame@gmail.com>
2025-12-31 11:36:19 +0530

    navbar

--- 13_UI-design-project/src/App.jsx
+++ 13_UI-design-project/src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white p-5 text-xl'>
-    App
+    <div>
     </div>
   )
 }
---
+++ React/Practice/11_navbar/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <div className='top'>
+    <div className='logo'>
+      <h1>Logo</h1>
+    </div>
+
+    <div className='nav-link'> 
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </div>
+  )
+}
+
+export default App
