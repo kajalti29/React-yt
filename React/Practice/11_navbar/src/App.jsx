464d106c1bd9b995ec8fb05cf74bf0f5aff2e995
kajalti@29 <kajaltisirame@gmail.com>
2025-12-31 15:12:40 +0530

    code commit

--- React/Practice/11_navbar/src/App.jsx
+++ React/Practice/11_navbar/src/App.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-  <div className='top'>
-    <div className='logo'>
-      <h1>Logo</h1>
-    </div>
+      <div className='navbar'>
+        <div className='logo'>MyLogo</div>
 
-    <div className='nav-link'> 
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-  </div>
+        <ul className='nav-links'>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul> 
+
+
+
+      </div>  
   )
 }
 
