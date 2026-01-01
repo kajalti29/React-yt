f27a2079b37307f3bb4c687025c64dbdf8013d22
kajalti@29 <kajaltisirame@gmail.com>
2026-01-01 09:07:12 +0530

    code commit

--- 13_UI-design-project/src/App.jsx
+++ 13_UI-design-project/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './Component/Section1/Navbar'
+import Page1Content from './Component/Section1/Page1Content'
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen w-full">
+      <Navbar />
+      <Page1Content />
     </div>
   )
 }
---
+++ 13_UI-design-project/src/Component/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className="h-screen w-full bg-blue-900 text-white text-xl">
+      Section 1
+    </div>
+  )
+}
+
+export default Section1
